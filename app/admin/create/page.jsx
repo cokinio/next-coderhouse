02eefd8f0cc19426b8893872ddd0a9eb8d2fc37e
Leandro Schlain <cokinio@gmail.com>
2023-12-07 15:53:35 -0300

    se agrego firebase

---
+++ app/admin/create/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <CreateForm />
+    </div>
+  )
+}
+
+export default page
---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC_4nD_acG8d8uDXm-dBqlgFcE-anMzMu0",
+    authDomain: "next-coder-403b4.firebaseapp.com",
+    projectId: "next-coder-403b4",
+    storageBucket: "next-coder-403b4.appspot.com",
+    messagingSenderId: "607394321456",
+    appId: "1:607394321456:web:e2ae9b941564a195ec09f5"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
